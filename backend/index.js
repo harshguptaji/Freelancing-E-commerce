3435ebf96cf6be3a428ae87f37804aa61d62bee1
Harsh Gupta <harshvaslas09752@gmail.com>
2026-04-07 15:05:02 +0530

    Updated Server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-// Staring Project
+import dotenv from "dotenv";
+
+dotenv.config({quiet:true});
+import express from "express";
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT - ${PORT}`);
+});
+
