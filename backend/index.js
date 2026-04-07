1998146b5e8c2c70a92919a9e61786f33cecb8be
Harsh Gupta <harshvaslas09752@gmail.com>
2026-04-07 15:26:50 +0530

    Database connected

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config({quiet:true});
 import express from "express";
-
-
+import { connectDB } from "./db/db.js";
 const app = express();
 
-
+connectDB();
 const PORT = process.env.PORT || 4000;
 
 
