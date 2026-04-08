d164604605f96aa4228710c9d43ac8eb7dcaf688
Harsh Gupta <harshvaslas09752@gmail.com>
2026-04-08 13:14:09 +0530

    Created User Model with Password hashing

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ import dotenv from "dotenv";
 dotenv.config({quiet:true});
 import express from "express";
 import { connectDB } from "./db/db.js";
+
+//Database connected
+connectDB();
+
 const app = express();
 
-connectDB();
-const PORT = process.env.PORT || 4000;
+// Middlewares
 
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT - ${PORT}`);
---
+++ backend/models/userModel.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+// const otp = Math.floor(10000 + Math.random() * 900000);
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    userEmail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userPassword: {
+        type: String,
+        required: true
+    },
+    userMobile: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userRole: {
+        type: String,
+        enum: ['Admin','User'],
+        default: "User",
+        required: true
+    },
+    userEmailOTP:{
+        type: String
+    },
+    userEmailOTPExpiry:{
+        type: Date
+    },
+    userMobileOTP:{
+        type: String
+    },
+    userMobileOTPExpiry:{
+        type: Date
+    },
+    userVerified:{
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    userCreated: {
+        type: Date,
+        default: Date.now
+    },
+    passwordForgot: {
+        type: Date
+    }
+});
+
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("userPassword")) return next();
+
+    this.userPassword = await bcrypt.hash(this.userPassword, 10);
+    next();
+})
+
+const User = mongoose.model("User", userSchema);
+export default User;
