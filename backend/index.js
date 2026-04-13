91a575d4803f8f9f4c82d3f85449b0e5b151a03b
Harsh Gupta <harshvaslas09752@gmail.com>
2026-04-13 18:26:37 +0530

    Add user routes and some middlewares

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config({quiet:true});
 import express from "express";
 import { connectDB } from "./db/db.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoutes.js";
 
 //Database connected
 connectDB();
@@ -10,6 +12,12 @@ connectDB();
 const app = express();
 
 // Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// ALL Routes
+app.use("/api/v1/user",userRoute);
 
 
 const PORT = process.env.PORT || 4000;
