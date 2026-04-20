7c02536f458b7e6ebc9a7f730af010ca967e2786
Harsh Gupta <harshvaslas09752@gmail.com>
2026-04-20 16:21:44 +0530

    Updated user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,30 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { allUser, forgotPassword, loginUserInfo, passwordChange, registerUser, sentOTP, updateLoginUserInfo, userById, userLogin, userLogout, userVerify } from "../controllers/userController.js";
+import { authorizeRoles, isAuthenthicated } from "../middlewares/authMiddleware.js";
 
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
+router.post("/login",userLogin);
+
+router.get("logout", userLogout);
+
+router.get("/all",isAuthenthicated, authorizeRoles("Admin"), allUser);
+
+router.get("profile", isAuthenthicated, loginUserInfo);
+
+router.put("forgotpassword", isAuthenthicated, passwordChange);
+
+router.post("verify",isAuthenthicated, userVerify);
+
+router.get("get/otp", isAuthenthicated, sentOTP);
+
+router.post("forgot-password", forgotPassword);
+
+router.put("edit/:id", isAuthenthicated, updateLoginUserInfo);
+
+router.get("/:id",isAuthenthicated, authorizeRoles("Admin"),userById);
 
 export default router;
